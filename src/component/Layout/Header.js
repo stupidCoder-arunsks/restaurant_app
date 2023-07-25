26eb618c460d1b158ad2666c6d9c70c8554404d0
Arunsks <arunsks57@gmail.com>
2023-07-25 11:46:01 +0530

    added header for restaurant app

---
+++ src/component/Layout/Header.js
@@ -0,0 +1,20 @@
+import { Fragment } from "react";
+import MealImage from "../../asset/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h2>React Meals</h2>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={MealImage} alt="A table full of delicious Meals!"></img>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
---
+++ src/component/Layout/HeaderCartButton.js
@@ -0,0 +1,19 @@
+import { Fragment } from "react";
+import classes from "./HeaderCartButton.module.css";
+import CartIcon from "../Cart/CartIcon";
+
+const HeaderCartButton = (props) => {
+  return (
+    <Fragment>
+      <button className={classes.button}>
+        <span className={classes.icon}>
+          <CartIcon />
+        </span>
+        <span>Cart</span>
+        <span className={classes.badge}>3</span>
+      </button>
+    </Fragment>
+  );
+};
+
+export default HeaderCartButton;
