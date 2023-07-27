1d4c3e198cbe96c2322f254fee42b2d19d85133b
Arunsks <arunsks57@gmail.com>
2023-07-27 10:23:18 +0530

    added meals details

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Fragment } from "react";
 import Header from "./component/Layout/Header";
+import Meals from "./component/Meals/Meals";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
